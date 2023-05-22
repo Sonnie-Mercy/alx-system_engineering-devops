9e5b53527889ec080cd8abf1cf19d9a307145f33
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-22 05:31:39 -0700

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,15 +3,26 @@
 A script that uses REST API to fetch employee TODO list progress.
 """
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}". format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    employee_id = argv[1]
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    # Retrieve employee details
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    employee_resp = requests.get(employee_url)
+    employee_data = employee_resp.json()
+    emp_name = employee_data.get("name")
+
+    # Retrieve employee TODO list
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_resp = requests.get(todos_url)
+    todos_data = todos_resp.json()
+
+    tasks = len(todos_data)
+    comp = [todo for todo in todos_data if todo.get("completed")]
+    num_comp = len(comp)
+
+    print(f"Employee {emp_name} is done with tasks({num_comp}/{tasks}):")
+    for task in comp:
+        print("\t", task.get("title"))
