20832c4393c46d288a02af0336064d77798a5e81
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-22 04:48:37 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Script that uses REST API for fetching employee TODO progress
-and exports data to CSV.
+Script that fetches data from the REST API for a particular employee ID
+and exports data to a JSON file.
 """
-import csv
+import json
 import requests
 import sys
 
@@ -20,11 +20,14 @@ if __name__ == "__main__":
     user = requests.get(user_endpoint).json()
     todos = requests.get(todo_endpoint, params={"userId": employee_id}).json()
 
-    with open('{}.csv'.format(employee_id), 'w', newline='') as csvfile:
-        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            row = [employee_id,
-                   user.get('username'),
-                   task.get('completed'),
-                   task.get('title')]
-            taskwriter.writerow(row)
+    tasks = []
+    for task in todos:
+        task_dict = {
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": user.get('username')
+        }
+        tasks.append(task_dict)
+
+    with open('{}.json'.format(employee_id), 'w') as jsonfile:
+        json.dump({employee_id: tasks}, jsonfile)
