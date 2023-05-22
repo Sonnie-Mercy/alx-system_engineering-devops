387b8614183d86b54d0c1975f0146e8c5f3593e9
Sonnie-Mercy <ndirangumercy4@gmail.com>
2023-05-22 04:59:49 -0700

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-"""
-Script that fetches data from the REST API for a particular employee ID
-and exports data to a JSON file.
-"""
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        exit(1)
-
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user_endpoint = "{}users/{}".format(url, employee_id)
-    todo_endpoint = "{}todos".format(url)
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    user = requests.get(user_endpoint).json()
-    todos = requests.get(todo_endpoint, params={"userId": employee_id}).json()
-
-    tasks = []
-    for task in todos:
-        task_dict = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": user.get('username')
-        }
-        tasks.append(task_dict)
-
-    with open('{}.json'.format(employee_id), 'w') as jsonfile:
-        json.dump({employee_id: tasks}, jsonfile)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
