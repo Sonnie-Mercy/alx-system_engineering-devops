b05cb18ee0bea0c70c14937a33cb0e1d5ca56448
root <ndirangumercy4@gmail.com>
2023-07-04 11:55:21 -0700

    an advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,11 +1,2 @@
 #!/usr/bin/env ruby
-log_file = ARGV[0]
-log_data = File.read(log_file)
-pattern = /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
-matches = log_data.scan(pattern)
-matches.each do |match|
-  sender = match[0]
-  receiver = match[1]
-  flags = match[2]
-  puts "#{sender},#{receiver},#{flags}"
-end
+puts ARGV[0].scan(/\[from:(\+?\w*)\]\s\[to:(\+?\w*)\]\s\[flags:(\S*)\]/).join(',')
