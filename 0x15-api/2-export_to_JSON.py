685d6b9a7e4d8b2d31a0ba3094a5c4984a59634a
sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-25 08:11:55 -0700

    task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" imports data in JSON format"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    u = requests.get(url + "users/{}".format(userId)).json()
+    uname = u.get("username")
+    task = request.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.json".format(userId), "w") as jsonfile:
+        json.dump({userId: [{
+            "task": t.get("title"),
+            "completed": t.get("completed"),
+            "username": uname
+            } for t in task]}, jsonfile)
