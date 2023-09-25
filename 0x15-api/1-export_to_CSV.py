d245c3324efeca049d0570f8bbe31087851ae4b4
sonnie-Mercy <ndirangumercy4@gmail.com>
2023-09-25 07:47:55 -0700

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using this REST API, for a given employee ID, returns
+information about his/her TODO list progress."""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    u = requests.get(url + "users/{}".format(userId)).json()
+    uname = u.get("username")
+    task = requests.get(url + "todos", params={"userId": userId}).json()
+
+    with open("{}.csv".format(userId), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            userId, uname, t.get("completed"), t.get("title")]) for t in task
